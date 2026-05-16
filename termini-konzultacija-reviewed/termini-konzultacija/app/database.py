9a606096c902b6eeed775698f68a5e8191afc70f
andelic-i <ivano6.andelic9@gmail.com>
2026-05-17 01:25:39 +0200

    Update database.py for environment-based configuration
    Prva promjena u database.py, dodan dotenv, ovisno na kojoj se bazi podataka radi potrebno promijeniti .env file (kostur nazvan .env_template)

--- termini-konzultacija-reviewed/termini-konzultacija/app/database.py
+++ termini-konzultacija-reviewed/termini-konzultacija/app/database.py
@@ -1,20 +1,37 @@
-from collections.abc import Generator
+#from collections.abc import Generator Takoder ostalo od kostura, mislim da je to antunov dio..?
+import os
 
-from sqlmodel import Session, SQLModel, create_engine
+from dotenv import load_dotenv
 
-from app.config import settings
+from sqlmodel import (
+    Session,
+    create_engine
+)
 
-engine = create_engine(settings.database_url, echo=False)
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+engine = create_engine(DATABASE_URL)
 
 
-def create_db_and_tables() -> None:
-    """Create database tables for local development/demo."""
+def get_session():
 
-    SQLModel.metadata.create_all(engine)
+    return Session(engine)
 
-
-def get_session() -> Generator[Session, None, None]:
-    """FastAPI dependency that provides a database session."""
-
-    with Session(engine) as session:
-        yield session
+#def get_session() -> Generator[Session, None, None]:
+#    """FastAPI dependency that provides a database session."""
+#
+#   with Session(engine) as session:
+#        yield session
+# Ostalo od kostura, po potrebi zamijeniti postojeći get_session() Opet, ako je ovo antun dio 
+# ali vrlo je moguce da sam ja lud, ovo ovako funkcionira no valjda moze biti bolje..? - Ivano 
